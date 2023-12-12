ec66d75aa4e56522f3181ffad78239661eaab2ce
UnicornSB <153204243+UnicornSB@users.noreply.github.com>
2023-12-12 11:00:43 -0500

    s

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 
 let btn=document.getElementById("includedContent");
-btn.addEventListener("click",function);
-
-
-$(function(){
+btn.addEventListener("click",function{
     $("#includedContent").load("index2.html"); 
-  });
+
+});
+
