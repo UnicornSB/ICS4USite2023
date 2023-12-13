7a8239506624a494765a9f5392ca00f1bae161d3
elizabethlifshin <153305161+elizabethlifshin@users.noreply.github.com>
2023-12-13 10:20:11 -0500

    hiii.css

---
+++ Pages/2-ICS4C/script.js
@@ -0,0 +1,29 @@
+.Unité1 {
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 20px;
+}
+
+.Unité2 {
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 20px;
+}
+
+.Unité3 {
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 20px;
+}
+
+.Unité4 {
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 20px;
+}
+
+.Unité5 {
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 20px;
+}
