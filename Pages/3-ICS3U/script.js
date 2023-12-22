d609cdff92f31487469ae1fa175e2c3cdc58fa84
flutepoint <153307171+flutepoint@users.noreply.github.com>
2023-12-22 11:01:11 -0500

    x

--- Pages/3-ICS3U/script.js
+++ Pages/3-ICS3U/script.js
@@ -3,3 +3,6 @@ var inc = 0;
 var margin = 0;
 var slider = document.getElementsByClassName("sliderwidth")[0];
 var itemDisplay = 0;
+if (screen.width > 990){
+    itemDisplay = document.getElementsByClassName("slider")[0].getAttribute
+}
