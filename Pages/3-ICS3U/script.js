994026364b78f3c18d0dff57fb50672d8c9f1a6b
flutepoint <153307171+flutepoint@users.noreply.github.com>
2024-01-08 13:16:32 -0500

    Variante de slider

--- Pages/3-ICS3U/script.js
+++ Pages/3-ICS3U/script.js
@@ -1,8 +1,40 @@
-var count = 0;
-var inc = 0;
-var margin = 0;
-var slider = document.getElementsByClassName("sliderwidth")[0];
-var itemDisplay = 0;
-if (screen.width > 990){
-    itemDisplay = document.getElementsByClassName("slider")[0].getAttribute
+// var count = 0;
+// var inc = 0;
+// var margin = 0;
+// var slider = document.getElementsByClassName("sliderwidth")[0];
+// var itemDisplay = 0;
+// if (screen.width > 990){
+//     itemDisplay = document.getElementsByClassName("slider")[0].getAttribute
+// }
+const initSlider = () => {
+    const imageList = document.querySelector(".slider-wrapper .image-list");
+    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+    const sliderScrollbar = document.querySelector(".container .slider-scrollbar");
+    const scrollbarThumb = sliderScrollbar.querySelector(".scrollbar-thumb");
+    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "prev-slide" ? - 1: 1;
+            const scrollAmount = imageList.clientWidth * direction;
+            imageList.scrollBy({ left: scrollAmount, behavior: "smooth"});
+        });
+    });
+
+    const handleSlideButtons = () => {
+        slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "block";
+        slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none" : "block";
+    }
+
+    const updateScrollThumbPosition = () => {
+        const scrollPosition = imageList.scrollLeft;
+        const thumbPosition = (scrollPosition / maxScrollLeft) * (sliderScrollbar.clientWidth - scrollbarThumb.offsetWidth)
+        scrollbarThumb.style.left = `${thumbPosition}px`;
+    }   
+
+    imageList.addEventListener("scroll", () => {
+        handleSlideButtons();
+        updateScrollThumbPosition();
+    } )
 }
+window.addEventListener("load", initSlider);
