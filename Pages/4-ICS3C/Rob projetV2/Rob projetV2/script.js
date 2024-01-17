cc93ecda1db280a81adb03884ee856daf7b3c13a
robleh123 <153305494+robleh123@users.noreply.github.com>
2024-01-17 10:11:58 -0500

    Add files via upload

---
+++ Pages/4-ICS3C/Rob projetV2/Rob projetV2/script.js
@@ -0,0 +1,66 @@
+const initSlider = () => {
+    const imageList = document.querySelector(".slider-wrapper .image-list");
+    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+    const sliderScrollbar = document.querySelector(".container .slider-scrollbar");
+    const scrollbarThumb = sliderScrollbar.querySelector(".scrollbar-thumb");
+    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+
+    // vérifie si l'image est actuellement visible
+    const isElementInViewport = (el) => {
+        const rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    };
+
+    
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "prev-slide" ? -1 : 1;
+            const scrollAmount = imageList.clientWidth * direction;
+            imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+        });
+    });
+
+    //affiche le bouton en fonction de la position
+    const handleSlideButtons = () => {
+        slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "flex";
+        slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none" : "flex";
+    };
+
+   
+    const updateScrollThumbPosition = () => {
+        const scrollPosition = imageList.scrollLeft;
+        const thumbPosition = (scrollPosition / maxScrollLeft) * (sliderScrollbar.clientWidth - scrollbarThumb.offsetWidth);
+        scrollbarThumb.style.left = `${thumbPosition}px`;
+    };
+
+   
+    imageList.addEventListener("scroll", () => {
+        updateScrollThumbPosition();
+        handleSlideButtons();
+    });
+
+    // Associe l'événement de clic aux boutons
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "prev-slide" ? -1 : 1;
+            const visibleSlide = direction === -1 ? imageList.querySelector(".slide:first-child") : imageList.querySelector(".slide:last-child");
+
+            // Vérifiez si la diapositive visible est dans la fenêtre
+            if (visibleSlide && isElementInViewport(visibleSlide)) {
+                // Récupérer le lien associé à la slide visible
+                const link = visibleSlide.querySelector('img').getAttribute('data-link');
+                
+                //Accédez au lien
+                window.location.href = link;
+            }
+        });
+    });
+};
+
+window.addEventListener("resize", initSlider);
+window.addEventListener("load", initSlider);
