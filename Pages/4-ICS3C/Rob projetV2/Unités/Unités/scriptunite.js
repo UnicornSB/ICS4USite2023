0c06001d959c50c81e6102ce226073e06d35162f
asdasfagh <153305810+asdasfagh@users.noreply.github.com>
2024-01-17 21:56:57 -0500

    x

---
+++ Pages/4-ICS3C/Rob projetV2/Unités/Unités/scriptunite.js
@@ -0,0 +1,7 @@
+// document.getElementById('button1').addEventListener('click', function() {
+//     window.location.href ="index.html";
+//   });
+
+document.getElementById('button1').addEventListener('click', function() {
+    window.location.href ="../index.html";
+  });
