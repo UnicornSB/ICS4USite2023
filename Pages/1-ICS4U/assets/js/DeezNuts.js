bc07b20a628e35bddc1f14aff440ff2465617e5f
IWillRobYourBloodline <153305429+IWillRobYourBloodline@users.noreply.github.com>
2024-01-19 21:52:04 -0500

    Add files via upload

---
+++ Pages/1-ICS4U/assets/js/DeezNuts.js
@@ -0,0 +1,20 @@
+const Btn = document.getElementsByClassName("item")
+
+
+for(i=0;i<Btn.length; i++){
+    Btn[3].addEventListener("click",event=>{
+        window.location.href='Unit 1/index.html';
+    })
+    Btn[4].addEventListener("click",event=>{
+        window.location.href='Unit 2/index (1).html';
+    })
+    Btn[5].addEventListener("click",event=>{
+        window.location.href='Unit 3/index (1).html';
+    })
+    Btn[6].addEventListener("click",event=>{
+        window.location.href='Unit 4/index (1).html';
+    })
+    Btn[7].addEventListener("click",event=>{
+        window.location.href='Unit 5/index (1).html';
+    })
+};
